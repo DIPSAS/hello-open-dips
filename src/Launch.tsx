89be3fbe64cdf9315f74f95bfda3343d34bd3e5b
Bjørn Fjukstad <bfj@dips.no>
2023-01-18 09:40:07 +0100

    launching 🚀

---
+++ src/Launch.tsx
@@ -0,0 +1,18 @@
+import FHIR from "fhirclient"
+
+const Launch = () => {
+
+        console.log("lol");
+
+        FHIR.oauth2.authorize({
+                "iss": "https://api.dips.no/fhir",
+                "redirectUri": "/app",
+                "client_id": "hello-open-dips-app",
+                "scope": "openid dips-fhir-r4 fhirUser patient/*.read offline_access",
+                "launch": "cdp2010024"
+        });
+
+        return null;
+}
+
+export default Launch; 
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Launch from './Launch';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+        document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+        <Router>
+                <Routes>
+                        <Route path='/' element={<Launch />} />
+                </Routes>
+                <App />
+
+        </Router>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
