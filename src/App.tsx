c76def6620a711ecb22bfc2a8ff021820ce431fd
Bjørn Fjukstad <bfj@dips.no>
2023-01-18 10:30:56 +0100

    get patient and say hello

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import FHIR from "fhirclient"
-import { useEffect } from 'react'
-
+import { useEffect, useState } from 'react'
 
 const App = () => {
+
+        const [patient, setPatient] = useState<any>();
 
         useEffect(() => {
                 FHIR.oauth2.ready()
                         .then(client => client.request({ "url": "/Patient/cdp2010025", "headers": { "dips-subscription-key": process.env.REACT_APP_DIPS_SUBSCRIPTION_KEY! } }))
-                        .then(console.log)
+                        .then(setPatient)
                         .catch(console.error);
         }, [])
 
+        useEffect(() => {
+                console.log(patient);
+        }, [patient])
+
         return (
                 <div className="App">
-
+                        <p>
+                                Hello {patient?.name[0].text}!
+                        </p>
                 </div>
         );
 }
