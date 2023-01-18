fd1551b49adb7cc9fb63de9b892fafb611feae6b
William Dalheim <wida@dips.no>
2023-01-18 10:54:49 +0100

    typescript patient type

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 import FHIR from "fhirclient"
 import { useEffect, useState } from 'react'
+import { R4 } from "@ahryman40k/ts-fhir-types";
 
 const App = () => {
+	const [patient, setPatient] = useState<R4.IPatient | undefined>();
 
-        const [patient, setPatient] = useState<any>();
+	useEffect(() => {
+		FHIR.oauth2
+			.ready()
+			.then((client) =>
+				client.request({
+					url: "/Patient/cdp2010025",
+					headers: { "dips-subscription-key": process.env.REACT_APP_DIPS_SUBSCRIPTION_KEY! },
+				})
+			)
+			.then(setPatient)
+			.catch(console.error);
+	}, []);
 
-        useEffect(() => {
-                FHIR.oauth2.ready()
-                        .then(client => client.request({ "url": "/Patient/cdp2010025", "headers": { "dips-subscription-key": process.env.REACT_APP_DIPS_SUBSCRIPTION_KEY! } }))
-                        .then(setPatient)
-                        .catch(console.error);
-        }, [])
+	useEffect(() => {
+		console.log(patient);
+	}, [patient]);
 
-        useEffect(() => {
-                console.log(patient);
-        }, [patient])
-
-        return (
-                <div className="App">
-                        <p>
-                                Hello {patient?.name[0].text}!
-                        </p>
-                </div>
-        );
-}
+	return <div className="App">{patient && <p>Hello {patient?.name![0].text}</p>}</div>;
+};
 
 export default App;
