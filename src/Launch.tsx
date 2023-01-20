2145dbe29c2ceaefa18c28dd6285d2d5221f6e23
Bjørn Fjukstad <bfj@dips.no>
2023-01-20 10:25:58 +0100

    allow set issuer in query params

--- src/Launch.tsx
+++ src/Launch.tsx
@@ -1,9 +1,17 @@
 import FHIR from "fhirclient"
+import { useSearchParams } from 'react-router-dom';
 
 const Launch = () => {
+        const [searchParams] = useSearchParams();
+
+        // Let user specify issuer (iss) in query param
+        let iss = searchParams.get("iss")
+        if (!iss) {
+                iss = "https://api.dips.no/fhir"
+        }
 
         FHIR.oauth2.authorize({
-                "iss": "https://api.dips.no/fhir",
+                "iss": iss,
                 "redirectUri": "/app",
                 "client_id": "hello-open-dips-app",
                 "scope": "openid dips-fhir-r4 fhirUser patient/*.read offline_access",
@@ -13,4 +21,4 @@ const Launch = () => {
         return null;
 }
 
-export default Launch; 
+export default Launch;
