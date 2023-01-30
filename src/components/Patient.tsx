2bb010a88786333e05bfa80c374a7cc57ab80e7f
hagu <hagu@dips.no>
2023-01-30 11:27:52 +0100

    Fetch documents and style changes

--- src/components/Patient.tsx
+++ src/components/Patient.tsx
@@ -7,6 +7,7 @@ import clientContext from "../context/clientContext";
 const Patient: React.FC = () => {
 	const [patient, setPatient] = useState<R4.IPatient | undefined>();
 	const [loading, setLoading] = useState<boolean>(false);
+	const [documents, setDocuments] = useState<R4.IBundle | undefined>();
 	const [error, setError] = useState<string | undefined>(undefined);
 
 	const { id } = useParams();
@@ -31,9 +32,27 @@ const Patient: React.FC = () => {
 						console.error
 					});
 			}
+			async function fetchDocuments() {
+				await client.request({
+					url: `/DocumentReference?patient=${id}`,
+					headers: { "dips-subscription-key": import.meta.env.VITE_DIPS_SUBSCRIPTION_KEY },
+				})
+					.then((documents) => {
+						setLoading(false);
+						setDocuments(documents);
+					})
+					.catch((error) => {
+						setLoading(false);
+						setError(error);
+						console.error
+					});
+			}
+			fetchDocuments();
 			fetchPatient();
 		}
 	}, [client])
+
+	console.log("Documents in Patient: " + documents?.total);
 
 	if (loading) {
 		return <div>Loading...</div>
@@ -56,6 +75,15 @@ const Patient: React.FC = () => {
 					</i>
 					<p className="card-name">{patient?.name![0].given} {patient?.name![0].family}</p>
 					<p>{patient?.name![0].given} {patient?.name![0].family} is a {patient?.gender!} patient born {patient?.birthDate!}. </p>
+				</div>
+			</div>
+
+			<div className="blue-info-card">
+				<div className="text-wrapper">
+					<i className="document-icon">
+					</i>
+					<p className="card-name">Documents</p>
+					<p>{patient?.name![0].given} {patient?.name![0].family} has {documents?.total} documents </p>
 				</div>
 			</div>
 		</div>
